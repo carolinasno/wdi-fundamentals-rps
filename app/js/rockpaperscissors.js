22aa2606b01de7fac006be5799810ef7c6ae60a3
Carolina No <carolina.s.no@gmail.com>
2015-11-08 15:06:22 -0500

    added winner code

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -25,14 +25,28 @@ function getPlayerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    return move || getInput();
+    if (move < 0.33) {
+        return "rock";
+    } else if move < 0.66) {
+        return "paper";
+    } else if move < 0.99) {
+        return "scissors";
+    } else {
+        return getInput();
 }
 
 function getComputerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-    return move || randomPlay();
+    if (move < 0.33) {
+        return "rock";
+    } else if move < 0.66) {
+        return "paper";
+    } else if move < 0.99) {
+        return "scissors";
+    } else {
+        return randomPlay();
 }
 
 function getWinner(playerMove,computerMove) {
@@ -40,8 +54,20 @@ function getWinner(playerMove,computerMove) {
     // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
-    /* YOUR CODE HERE */
-    return winner;
+    if (playerMove < 0.33 + computerMove < 0.99) {
+        return winner "player";
+    } else if (playerMove < 0.99 + computerMove < 0.66) {
+        return winner "player";
+    } else if (playerMove < 0.66 + computerMove < 0.33) {
+        return winner "player";
+    } else if (computerMove < 0.33 + playerMove < 0.99) {
+        return winner "computer";
+    } else if (computerMove < 0.99 + playerMove < 0.66) {
+        return winner "computer";
+    } else if (computerMove < 0.66 + playerMove < 0.33) {
+        return winner "computer";
+    } else {
+        return winner "tie";
 }
 
 function playToFive() {
